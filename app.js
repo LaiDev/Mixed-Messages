105da190e1a9d0a0541611db6453a707226fde49
Aalaizha <l.bnjamin0309@gmail.com>
2022-07-26 11:19:42 -0400

    Add functionality for randomly generated message to display on UI

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-//Each function holds an array of multiple strings. When called the randomIndex whill randomly select and return a value from the array.
+//Each function holds an array of multiple strings. When called, the randomIndex will randomly select and return a value from the array.
 
 const who = () => {
-    let whoShould = ["You should ", "You and your family should ", "You and your friends should "] 
+    let whoShould = ["You should", "You and your family should", "You and your friends should", "You and your dog should", "You and your cat should"]
     let randomWho = Math.floor(Math.random() * whoShould.length);
-    return whoShould[randomWho]}
-    
+    return whoShould[randomWho]
+}
+
 const what = () => {
-    let whatActivity = ["go hiking ", "plan a camping trip ", "go surfing ", "swim with the dolphins ", "go to an amusment park "]
+    let whatActivity = ["go hiking", "plan a camping trip", "go surfing", "swim with the dolphins", "go to an amusment park", "go bike riding"]
     let randomActivity = Math.floor(Math.random() * whatActivity.length);
-    return whatActivity[randomActivity]}
-    
+    return whatActivity[randomActivity]
+}
+
 const when = () => {
-    let whenToGo = ["tommorrow ", "this evening ", "next week ", "next month ", "next year ", "in two weeks "] 
+    let whenToGo = ["tommorrow", "this evening", "next week", "next month", "next year", "in two weeks"]
     let randomDate = Math.floor(Math.random() * whenToGo.length);
-    return whenToGo[randomDate]}
-    
-    //combineMessages() will return all of the individual message parts into one complete sentence.
-    
-const combineMessage = () =>{return who() + what() + when()};
-    
-combineMessage();
+    return whenToGo[randomDate]
+}
+
+//combineMessages() will combine all of the individual message parts into one complete sentence.
+
+const combineMessage = () => {
+    let message = who() + " " + what() + " " + when();
+    return message
+};
+
+
+//Each time the user clicks the button, a new message will display on the screen
+
+let mesageButton = document.getElementById("generateMessage");
+
+mesageButton.onclick = () => {
+
+    document.querySelector("h1").innerText = combineMessage();
+
+}
